7b40d47f1c12b3f7a9247166ffe525b65a6df842
agustin-pasten <aopasten@miuandes.cl>
2021-04-13 10:14:23 -0400

    se elimino la clase desk_of_cards y sus metodos se incluyero en la clase card

--- card.py
+++ card.py
@@ -1,7 +1,11 @@
+import random
+
 class Card:
 
     #constantes
-    List_h = []
+    LIST_TYPE_CARDS = []
+    LIST_RANDOM_CARDS = []
+    LIST_RANDOM_NUMBERS = []
 
 
     #constructor
@@ -22,6 +26,28 @@ class Card:
             
 
     #metodos
+    def deck_cards(self):
+        if self.__name_card == "Deck":
+                self.LIST_TYPE_CARDS = ["Duque","Duque","Duque","Asesino","Asesino","Asesino",
+                     "Capitán","Capitán","Capitán","Embajador","Embajador","Embajador",
+                     "Condesa","Condesa","Condesa"]
+                
+    def deck_random_cards(self):
+        if self.__name_card == "Deck":
+            contador = 0
+            while True:
+                i = random.randint(0,len(self.LIST_TYPE_CARDS)-1)
+                if i not in self.LIST_RANDOM_NUMBERS:
+                    self.LIST_RANDOM_CARDS.append(self.LIST_TYPE_CARDS[i])   
+                    self.LIST_RANDOM_NUMBERS.append(i)
+                    contador +=1
+                if contador ==15:
+                    break
+            print("the cards were randomly shuffled")
+            return self.LIST_RANDOM_CARDS
+                
+            
+
 
 
 
--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 from card import Card
-from deck_of_cards import Deck_of_cards
 from person import Person
 
 
-def three_players():
-    print("hola")
+def three_players(card1):
+    player1 = Person("Player1")
+    player2 = Person("Player2")
+    player3 = Person("Player1")
+    print(card1.name_card)
 
 
 def four_players():
     print("chao")
 
 def main():
-    
 
+    deck = Card("Deck")
     card1 = Card("Duque")
     card2 = Card("Duque")
     card3 = Card("Duque")
@@ -28,17 +30,18 @@ def main():
     card13 = Card("Condesa")
     card14 = Card("Condesa")
     card15 = Card("Condesa")
-  
-    deck = Deck_of_cards(int(15))
+
+    print(deck.name_card)
+    deck.deck_cards()
     print(deck.LIST_TYPE_CARDS)
-    deck.random_cards()
-    print(len(deck.LIST_TYPE_CARDS))
+    deck.deck_random_cards()
     print(deck.LIST_RANDOM_CARDS)
 
+ 
 
     number_players = int(input("how many players will play this game? 3 or 4 : "))
     if number_players == 3:
-        three_players()
+        three_players(card1)
     elif number_players ==4:
         four_players()
 
